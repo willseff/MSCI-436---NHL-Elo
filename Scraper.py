b95dfb776f0db460c1f3100fbb4103bafc4c1cef
Will <w255li@edu.uwaterloo.ca>
2019-03-15 15:06:50 -0400

    Scraper finished
    almost works

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,23 @@
 # pip install hockey scraper
-import hockey_scraper
+import pandas as pd
 
-hockey_scraper.scrape_date_range('2016-10-10', '2016-10-12', False)
+df_lists = []
+
+for year in range (2000,2010):
+
+	if year == 2005:
+		print("2005 was the lockout")
+
+	else:
+		url = r'https://www.hockey-reference.com/leagues/NHL_' + str(year) + r'_games.html' 
+
+		df_lists.append(pd.read_html(url)[0])
+
+		print (str(year) + " scraped")
+
+
+df = pd.concat(df_lists)
+
+df.to_csv('game outcomes.csv')
+
+
