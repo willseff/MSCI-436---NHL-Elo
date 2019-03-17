df9473d8117b559fea64bb7c30a34842bcff0c19
Will <w255li@edu.uwaterloo.ca>
2019-03-16 20:51:05 -0400

    imported label encoder

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,12 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("game outcomes.csv")
 
 df.drop(labels=[ 'Att.', 'LOG', 'Notes'], inplace=True, axis=1)
+
+le = LabelEncoder()
+
+df[]
 
 df.to_csv('game outcomes.csv')
