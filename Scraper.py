1bc26f3d4ed9c802f25938afedeefcb14d0ff6e3
Will <w255li@edu.uwaterloo.ca>
2019-03-18 23:06:33 -0400

    Combined preprocessing and scraper code

--- Scraper.py
+++ Scraper.py
@@ -20,7 +20,40 @@ class scraper:
 
 				print (str(year) + " scraped")
 
-		self.df = pd.concat(df_lists)
+
+		df = pd.concat(df_lists)
+		teamEncodings = pd.read_csv('Team Encodings.csv')
+
+		#df.drop(labels=[ 'Att.', 'LOG', 'Notes'], inplace=True, axis=1)
+
+		df['homeTeamEncode'] = 0
+		df['visitorTeamEncode'] = 0
+
+		df['Date'] = pd.to_datetime(df['Date'])
+
+
+		for gameRow in df.itertuples():
+			for teamRow in teamEncodings.itertuples():
+				if gameRow.Visitor == teamRow.Team:
+
+					idx = gameRow.Index
+
+					df.at[idx, 'visitorTeamEncode'] = teamRow.Encoding
+
+		print('Visiting Teams Encoded')			
+
+		for gameRow in df.itertuples():
+			for teamRow in teamEncodings.itertuples():
+				if gameRow.Home == teamRow.Team:
+
+					idx = gameRow.Index
+
+					df.at[idx, 'homeTeamEncode'] = teamRow.Encoding
+
+		print('Home Teams Encoded')			
+
+
+		self.df = df [['Date','homeTeamEncode', 'G','visitorTeamEncode', 'G.1', 'Unnamed: 5']]
 
 
 
