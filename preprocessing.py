a52cce25533f6abae548a349f06e0a546b685673
Will <w255li@edu.uwaterloo.ca>
2019-03-18 22:28:20 -0400

    Encoding to teams added

--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,34 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("game outcomes.csv")
+teamEncodings = pd.read_csv('Team Encodings.csv')
 
-df.drop(labels=[ 'Att.', 'LOG', 'Notes'], inplace=True, axis=1)
+#df.drop(labels=[ 'Att.', 'LOG', 'Notes'], inplace=True, axis=1)
 
-le = LabelEncoder()
+df['homeTeamEncode'] = 0
+df['visitorTeamEncode'] = 0
 
-df[]
+
+for gameRow in df.itertuples():
+	for teamRow in teamEncodings.itertuples():
+		if gameRow.Visitor == teamRow.Team:
+
+			idx = gameRow.Index
+
+			df.at[idx, 'visitorTeamEncode'] = teamRow.Encoding
+
+
+for gameRow in df.itertuples():
+	for teamRow in teamEncodings.itertuples():
+		if gameRow.Home == teamRow.Team:
+
+			idx = gameRow.Index
+
+			df.at[idx, 'homeTeamEncode'] = teamRow.Encoding
+
+
+df = df ['Date', 'homeTeamEncode','G','visitorTeamEncode', 'G.1', 'Unnamed: 5']
+
+
 
 df.to_csv('game outcomes.csv')
