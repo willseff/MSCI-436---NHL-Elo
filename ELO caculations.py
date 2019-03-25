64398fd99a601567b30eddf42bd7a2986b590c94
Will <w255li@edu.uwaterloo.ca>
2019-03-25 18:36:02 -0400

    Update ELO caculations.py

--- ELO caculations.py
+++ ELO caculations.py
@@ -34,15 +34,13 @@ def update_end_of_season(elos):
     elos -= diff_from_mean/3
     return elos
 
-df_reg = pd.read_csv("game.csv")
-for row in df_reg.itertuples():
-    idx = row.Index
+df_reg = pd.read_csv("game outcomes.csv")
+# for row in df_reg.itertuples():
+#     idx = row.Index
 
-    s = df_reg.at[idx,'date_time']
+#     s = df_reg.at[idx,'date_time']
 
-df_reg.drop(labels=[ 'home_rink_side_start', 'venue', 'venue_link', 'venue_time_zone_id','venue_time_zone_offset','venue_time_zone_tz'], inplace=True, axis=1)
-df_reg.sort_values(by=['date_time'], inplace=True)
-df_reg.head()
+
 mean_elo = 1500
 elo_width = 400
 k_factor = 64
@@ -52,38 +50,38 @@ df_reg['w_elo_after_game'] = 0
 df_reg['l_elo_after_game'] = 0
 data = [1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500]
 team_elos = pd.DataFrame(data)
-print(team_elos.iloc[6])
+
 current_season = df_reg.at[0, 'season']
 for row in df_reg.itertuples():
     if row.season != current_season:
         # Check if we are starting a new season. 
         # Regress all ratings towards the mean
         team_elos = update_end_of_season(team_elos)
-        # Write the beginning of new season ratings to a dict for later lookups.
         current_season = row.season
         
     idx = row.Index
-    if row.outcome == 'away win REG' or 'away win OT':
-        w_id = row.away_team_id-1
-        l_id = row.home_team_id-1
+    if row.homeTeamGoals < row.visitorTeamGoals :
+        w_id = row.visitorTeamEncode
+        l_id = row.homeTeamEncode
         
-    elif row.outcome == 'home win REG' or 'home win OT':
-        w_id = row.home_team_id-1
-        l_id = row.away_team_id-1
+    else:
+        w_id = row.homeTeamEncode
+        l_id = row.visitorTeamEncode
         
     # Get current elos
-    w_elo_before = team_elos.iloc[w_id]
-    l_elo_before = team_elos.iloc[l_id]
+    w_elo_before = team_elos.iloc[w_id-1]
+    l_elo_before = team_elos.iloc[l_id-1]
     # Update on game results
     w_elo_after, l_elo_after = update_elo(w_elo_before, l_elo_before)
     
       # Save updated elos
     df_reg.at[idx, 'w_elo_after_game'] = w_elo_after
     df_reg.at[idx, 'l_elo_after_game'] = l_elo_after
-    team_elos.iloc[w_id] = w_elo_after
-    team_elos.iloc[l_id] = l_elo_after
+    team_elos.iloc[w_id-1] = w_elo_after
+    team_elos.iloc[l_id-1] = l_elo_after
 
 
 df_reg.to_csv('out.csv')
+team_elos.to_csv('team elos.csv')
 
 
