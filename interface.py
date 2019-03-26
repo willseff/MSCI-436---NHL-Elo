82e6e7de97d99100db00bf3a463971ae52f9873d
Will <w255li@edu.uwaterloo.ca>
2019-03-25 23:21:12 -0400

    added titles

--- interface.py
+++ interface.py
@@ -3,11 +3,19 @@ from tkintertable import TableCanvas, TableModel
 
 root = Tk()
 
+one = Label(root,text = 'NHL Elo', bg ='blue', fg = 'white')
+one.pack()
+
 tframe = Frame(root)
 tframe.pack()
-table = TableCanvas(tframe)
+table = TableCanvas(tframe, rowheaderwidth=0)
 table.importCSV('team elos.csv')
 table.show()
+
+two = Label(root,text = 'Upcoming Games', bg ='blue', fg = 'white')
+two.pack()
+
+#make a df with upcoming games and chance of win here
 
 root.mainloop()
 
