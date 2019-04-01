a0f3fe5759daaac217480ea071bd4d2fc5abb5ae
Will <w255li@edu.uwaterloo.ca>
2019-04-01 09:52:11 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,31 +2,36 @@ from Tkinter import *
 from tkintertable import TableCanvas, TableModel
 import pandas as pd
 
+class interface:
+	
+	def __init__(self):
+
+		root = Tk()
+
+		root.title('NHL Elo Predictions')
+
+		one = Label(root,text = 'NHL Elo', bg ='blue', fg = 'white')
+		one.pack()
+
+		tframe = Frame(root)
+		tframe.pack()
+		table = TableCanvas(tframe, rowheaderwidth=0)
+		table.importCSV('team elos.csv')
+		table.sortTable(columnName='elo')
+		colIndex = 1
+		table.show()
+
+		two = Label(root,text = 'Games Today', bg ='blue', fg = 'white')
+		two.pack()
+
+		tframe2 = Frame(root)
+		tframe2.pack()
+		table2 = TableCanvas(tframe2, rowheaderwidth=0)
+		table2.importCSV('test.csv')
+		table2.show()
+
+		root.mainloop()
 
 
-root = Tk()
-
-root.title('NHL Elo Predictions')
-
-one = Label(root,text = 'NHL Elo', bg ='blue', fg = 'white')
-one.pack()
-
-tframe = Frame(root)
-tframe.pack()
-table = TableCanvas(tframe, rowheaderwidth=0)
-table.importCSV('team elos.csv')
-table.sortTable(columnName='elo')
-colIndex = 1
-table.show()
-
-two = Label(root,text = 'Games Today', bg ='blue', fg = 'white')
-two.pack()
-
-tframe2 = Frame(root)
-tframe2.pack()
-table2 = TableCanvas(tframe2, rowheaderwidth=0)
-table2.importCSV('test.csv')
-table2.show()
-
-root.mainloop()
+interface()
 
