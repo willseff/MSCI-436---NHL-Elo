f79837e962b789cf77d721de1fe7d34c57e9aa56
Will <w255li@edu.uwaterloo.ca>
2020-01-01 14:24:00 -0500

    added app routes

--- App/app.py
+++ App/app.py
@@ -1,18 +1,48 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect
 from db_model import *
+import pandas as pd
 
 db = SqliteDatabase('elos.db')
 db.connect()
+# query db for the elos ratings
 query = Elos.select()
 # dict comprenshion!!!
 elo_ratings = {row.team: row.elo for row in query}
+
+# query db for the predictions
+query = Prediction.select()
+#list comprehension to turn query into list
+predictions_list = [[row.id, 
+                     row.date, 
+                     row.visitor, 
+                     row.visitor_win, 
+                     row.home,
+                     row.home_win] for row in query]
+
+# then turn list in to df
+predictions = pd.DataFrame(predictions_list)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html', elo_ratings=elo_ratings)
+	return render_template('index.html', 
+							elo_ratings=elo_ratings)
 
-app.run(debug=True, host='0.0.0.0', port=8000)
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
+@app.route('/home')
+def home():
+	return redirect('/')
+
+@app.route('/predictions')
+def predictions():
+	return render_template('predictions.html', 
+						   predictions=predictions)
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=8000)
 
